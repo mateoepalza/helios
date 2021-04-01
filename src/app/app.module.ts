0f89f753754827820523c8f0db034a61a60b28dd
Mateo Epalza <mateoepalzaramirez@gmail.com>
2021-04-01 01:05:06 -0500

    header and sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { SandBoxComponent } from './sand-box/sand-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SideBarComponent,
+    SandBoxComponent
   ],
   imports: [
     BrowserModule
