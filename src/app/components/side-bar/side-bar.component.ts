b84c3f8c1b58e1294b5274b699aad6c7bd0c0623
Mateo Epalza <mateoepalzaramirez@gmail.com>
2021-04-02 17:21:10 -0500

    sidebar fixed and animation added

--- src/app/components/side-bar/side-bar.component.ts
+++ src/app/components/side-bar/side-bar.component.ts
@@ -7,11 +7,20 @@ import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/co
 })
 export class SideBarComponent implements OnInit {
 
+  @ViewChild('sideBar') sideBar: ElementRef;
+  toggle = true;
+
   constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void {
   }
 
   onToggle() {
+    if(this.toggle){
+      this.renderer.addClass(this.sideBar.nativeElement, 'hide');
+    }else{
+      this.renderer.removeClass(this.sideBar.nativeElement, 'hide')
+    }
+    this.toggle = !this.toggle;
   }
 }
