fa726111cd05a095a65dbf5eb8dfcd7becc1f7de
Mateo Epalza <mateoepalzaramirez@gmail.com>
2021-04-01 22:50:43 -0500

    header side completed

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  toggle = false;
+  @ViewChild('subMenu') submenu: ElementRef;
+
+  constructor(private render: Renderer2) { }
 
   ngOnInit(): void {
+  }
+
+  onToggle() {
+    if (this.toggle) {
+      this.render.removeClass(this.submenu.nativeElement, 'show-sub');
+    } else {
+      this.render.addClass(this.submenu.nativeElement, 'show-sub');
+    }
+    this.toggle = !this.toggle;
   }
 
 }
